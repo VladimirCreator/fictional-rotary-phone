dd195c7f399f822e22fb470739575acc0d8eff40
Vladimir <49140851+VladimirCreator@users.noreply.github.com>
2024-02-23 04:28:16 +0000

    feat: add a dialog with a form

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 import './index.css'
+
+/** @type {HTMLFormElement|null} */
+const form = document.getElementById('form')
+
+/** @param {SubmitEvent} event */
+const handleSubmit = event => { event.preventDefault()
+	/** @type {HTMLFormElement} */
+	const currentTarget = event.currentTarget
+	if (!currentTarget) {
+		return
+	}
+	/** Словарь, который содержит пары ключ/значение, где
+		* ключ является именем поля, а
+		* значение является значением поля.
+	*/
+	const dictionary = Array.from(currentTarget.elements, element => [element.name, element.value])
+}
+
+form.addEventListener('submit', handleSubmit)
